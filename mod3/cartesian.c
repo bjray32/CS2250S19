5b98a04c3f93fef5e4008d57c0f7adc742d9a6f5
Brenton Ray <brentonray@mail.weber.edu>
2019-01-24 08:43:00 -0700

    Initial Commit

---
+++ mod3/cartesian.c
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cartesian.c
+ *
+ *    Description:  Calculate the cartesian coordinates based:
+ *                  radius and theta angle (degrees)
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 08:35:54 AM
+ *       Revision:  none
+ *       Compiler:  gcc cartesian.c -o cartesian.out -lm
+ *
+ *         Author:  Brenton Ray (), brentonray@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    double radius;
+    
+    // 1) Capture user input for radius and theta (degrees)
+    printf("Input Radius: ");
+    scanf("%lf", &radius);
+    printf("
+    // 2) Convert the theta angle to radians
+    // 3) Calculate your cartesian coordinates
+    // 4) Print the result
+
+    return 0;
+}
+// Function Definitions
+
+
