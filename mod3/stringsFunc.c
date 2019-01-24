e5913d994f4437a765ea83a382a29c5d026c878d
Brenton Ray <brentonray@mail.weber.edu>
2019-01-24 09:22:14 -0700

    string copy stuff

---
+++ mod3/stringsFunc.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  stringsFunc.c
+ *
+ *    Description:  Learn some string functions
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 09:08:40 AM
+ *       Revision:  none
+ *       Compiler:  gcc stringsFunc.c -o stringsFunc.out
+ *
+ *         Author:  Brenton Ray (), brentonray@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <string.h>            // for string manupulation operations
+
+
+int main()
+{
+    char first[50];
+    char last[50];
+    char fullName[100];  //could all be on one line
+
+    printf("Input First and Last Name: ");
+    scanf("%s %s", first, last);
+        printf("Hi [%s] [%s]\n", first, last);
+strcpy(fullName, first);
+printf("Your Full name is: [%s]\n", fullName);
+
+    return 0;
+}
+// Function Definitions
+
+
