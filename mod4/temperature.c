692f09b15e1a1dbbc3ab8244f72ae20e4da4f7f2
Brenton Ray <brentonray@mail.weber.edu>
2019-01-29 09:45:34 -0700

    added statement of what phase water is in

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -16,11 +16,17 @@
  * =====================================================================================
  */
 #include <stdio.h>
+
+// Constants
+
 //const int CELSIUS = 0;
 //const int FAHRENHEIT = 1;
 #define CELSIUS 0
 #define FAHRENHEIT 1
-
+#define BOIL_CEL 100
+#define BOIL_FAHR 212
+#define FREEZE_CEL 0
+#define FREEZE_FAHR 32
 // The above constants are equivalent
 
 // Function Prototypes
@@ -41,18 +47,18 @@ int main()
     out_temp = 0;
     if (choice == CELSIUS)
     {
-    printf("Enter the temperature in Celsius: ");
-    scanf("%lf", &in_temp);
-    // Convert from celsius to Fahrenheit
-    out_temp = (in_temp * 9.0/5.0) + 32;
-    printf("Your temperature in Fahrenheit is: %lf\n", out_temp);
+        printf("Enter the temperature in Celsius: ");
+        scanf("%lf", &in_temp);
+        // Convert from celsius to Fahrenheit
+        out_temp = (in_temp * 9.0/5.0) + 32;
+        printf("Your temperature in Fahrenheit is: %lf\n", out_temp);
     }
     else if (choice == FAHRENHEIT)
     {
-    printf("Enter the temperature in Fahrenheit: ");
-    scanf("%lf", &in_temp);
-    out_temp = (in_temp - 32) * 5.0/9.0;
-    printf("Your temperature in Celsius is: %lf\n", out_temp);
+        printf("Enter the temperature in Fahrenheit: ");
+        scanf("%lf", &in_temp);
+        out_temp = (in_temp - 32) * 5.0/9.0;
+        printf("Your temperature in Celsius is: %lf\n", out_temp);
     }
     else
     {
@@ -60,8 +66,43 @@ int main()
         printf("Sorry, you did not enter 0 or 1\nAdios amigo\n");
         return 0;
     }
+
+    // Task2:
+    // Test for boiling point, freezing point, liquid point
+
+    if (choice == CELSIUS)
+    {
+        if(in_temp >= BOIL_CEL)
+        {
+            printf("Your water is boiling\n");
+        }
+        // && -> AND
+        else if (in_temp > FREEZE_CEL && in_temp < BOIL_CEL)
+        {
+            printf("Your water is liquid\n");
+        }
+        else
+        {
+            printf("Your water is ice\n");
+        }
+    }
+    else // FAHRENHEIT
+    {
+        if(in_temp >= BOIL_FAHR)
+        {
+            printf("Your water is boiling\n");
+        }        
+        else if (in_temp > FREEZE_FAHR && in_temp < BOIL_FAHR)
+        {
+            printf("Your water is liquid\n");
+        }
+        else
+        {
+            printf("Your water is ice\n");
+        }
+    }
+
     return 0;
 }
 // Function Definitions
-
 
