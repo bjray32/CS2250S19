9b15c06e863c2fb81bf205959899b892f8645856
Brenton Ray <brentonray@mail.weber.edu>
2019-01-31 09:37:04 -0700

    days of month

--- mod4/month_day.c
+++ mod4/month_day.c
@@ -23,18 +23,56 @@ int main()
 {
 
     // 1) Capture user input
-    int month;
+    int month, day;
 
     printf("Enter Month (1-12): ");
     scanf("%d", &month);
+    printf("Enter Day(1-31): ");
+    scanf("%d", &day);
 
     // 2) Use IF to validate month
     if(month > 12 || month < 1)
     {
         printf("You have entered an invalid month\n");
-//            return 1;
+            return 1;
     }
     // 3) Use SWITCH to validate day of the month
+    switch(month)
+    {
+        // Moths with 31 days
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            if(day > 31 || day < 1)
+            {
+                printf("Invalid day\n");
+                return 1;
+            }
+            break;
+        // Months with 30 days
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if(day > 30 || day < 1)
+            {
+                printf("Invalid day\n");
+                return 1;
+            }
+            break;
+        // February
+        case 2:
+            if(day > 28 || day < 1)
+            {
+                printf("Invalid day\n");
+                return 1;
+            }
+            break;
+    }
 
 
 
