eda28fd1b3167f5f4e9c7a0ab479a1cd807f7417
Brenton Ray <brentonray@mail.weber.edu>
2019-01-31 08:38:41 -0700

    inital commit

---
+++ mod4/angle.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  angle.c
+ *
+ *    Description:  Calculate the quadrant based on input angle
+ *                  Quadrant I: angle between 0 and 90
+ *                  Quadrant II: angle between 90 and 180
+ *                  Quadrant III: angle between 180 and 270
+ *                  Quadrant IV: angle between 270 and 360
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 08:35:03 AM
+ *       Revision:  none
+ *       Compiler:  gcc angle.c -o angle.out
+ *
+ *         Author:  Brenton Ray (), brentonray@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    int angle;
+
+    printf("Enter an angle: ");
+    scanf("%d", &angle);
+    return 0;
+}
+// Function Definitions
+
+
