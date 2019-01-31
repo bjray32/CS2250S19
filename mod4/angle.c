1073529b4d2080b948c1ba3a41aa15f41498fb61
Brenton Ray <brentonray@mail.weber.edu>
2019-01-31 09:16:25 -0700

    values on axis

--- mod4/angle.c
+++ mod4/angle.c
@@ -8,6 +8,10 @@
  *                  Quadrant II: angle between 90 and 180
  *                  Quadrant III: angle between 180 and 270
  *                  Quadrant IV: angle between 270 and 360
+ *                  If angle is 0, then print positive X axis
+ *                  If angle is 90, then print positive Y axis
+ *                  If angle is 180, then print negative X axis
+ *                  If angle is 270, then print negative Y axis
  *
  *        Version:  1.0
  *        Created:  01/31/2019 08:35:03 AM
@@ -60,7 +64,26 @@ int main()
     else if (angle > 270 && angle < 360)
     {
         printf("Angle is in Quadrant IV\n");
-    }    
+    }
+    else if (angle == 0 || angle == 90
+            || angle == 180 || angle == 270)
+    {
+        switch(angle)
+        {
+        case 0:
+            printf("Angle is on positive X-Axis\n");
+            break;
+        case 90:
+            printf("Angle is on positive Y-Axis\n");
+            break;
+        case 180:
+            printf("Angle is on negative X-Axis\n");
+            break;
+        case 270:
+            printf("Angle is on negative Y-Axis\n");
+            break;
+        }
+    }
     else
     {
         printf("\nIncorrect angle\n");
