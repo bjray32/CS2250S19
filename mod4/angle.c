c43c4fa877c6e73bd15ec7cdbd6b050707b42cda
Brenton Ray <brentonray@mail.weber.edu>
2019-01-31 09:03:50 -0700

    negative and values passing 360

--- mod4/angle.c
+++ mod4/angle.c
@@ -12,7 +12,7 @@
  *        Version:  1.0
  *        Created:  01/31/2019 08:35:03 AM
  *       Revision:  none
- *       Compiler:  gcc angle.c -o angle.out
+ *       Compiler:  gcc angle.c -o angle.out -lm
  *
  *         Author:  Brenton Ray (), brentonray@mail.weber.edu
  *   Organization:  WSU
@@ -20,6 +20,8 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <stdlib.h>    // for regular abs
+#include <math.h>
 
 // Constants
 
@@ -33,6 +35,37 @@ int main()
 
     printf("Enter an angle: ");
     scanf("%d", &angle);
+    // Check for angles greater than 360
+    //angle = angle % 360;
+    // angle = sqrt(pow(angle % 360, 2));
+    angle = angle % 360;
+
+    if(angle < 0)
+    {
+        angle = abs(360 + angle);
+    }
+
+    if (angle > 0 && angle <90)
+    {
+        printf("Angle is in Quandrant I\n");
+    }
+    else if (angle > 90 && angle < 180)
+    {
+        printf("Angle is in Quandrant II\n");
+    }
+    else if (angle > 180 && angle < 270)
+    {
+        printf("Angle is in Quadrant III\n");
+    }
+    else if (angle > 270 && angle < 360)
+    {
+        printf("Angle is in Quadrant IV\n");
+    }    
+    else
+    {
+        printf("\nIncorrect angle\n");
+    }
+    
     return 0;
 }
 // Function Definitions
