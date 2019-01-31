418dc11cbdc5f7268953a353381d0784f8df010d
Brenton Ray <brentonray@mail.weber.edu>
2019-01-31 09:19:28 -0700

    inital commit

---
+++ mod4/month_day.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  Take month and day from user, then verify entries are valid.
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:17:13 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o month_day.out -lm
+ *
+ *         Author:  Brenton Ray (), brentonray@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+
+// Main Function
+int main()
+{
+    // 1) Capture user input
+    
+    // 2) Use IF to validate month
+
+    // 3) Use SWITCH to validate day of the month
+
+
+
+    return 0;
+}
+// Function Definitions
+
+
