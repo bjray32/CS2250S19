07432d8128d0f2300c5a04ae893e40a57504d4e0
Brenton Ray <brentonray@mail.weber.edu>
2019-02-05 08:57:04 -0700

    fixed float

--- mod5/firstwhile.c
+++ mod5/firstwhile.c
@@ -26,16 +26,24 @@
 int main()
 {
     // Task 1: Calculate the average of user input values
+    // Task 2: Validate user input for 0 to 100 0nly
     int count = 0;
     float grade, avg;
     float total = 0;
     while(count < MAXCOUNT)
     {
+        if ((grade >= 0) && (grade <= 100))
+        {
         printf("\nEnter %d hw grade(0-100) ", count +1);
         scanf("%f", &grade);
 
         total += grade;  // add up grade 
         count++;        // update test condition: sentinel
+        }
+        else
+        {
+            continue;
+        }
     }
     avg = total/MAXCOUNT;
     printf("Your avg is [%6.2f]\n", avg);
