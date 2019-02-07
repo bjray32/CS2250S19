1809b8f30b48100b3df8ea06f9806cfe44fc2f89
Brenton Ray <brentonray@mail.weber.edu>
2019-02-07 09:17:50 -0700

    add guess messages, also changed limits to 5 chances and 100 numnerlimit

--- mod5/guessNumber.c
+++ mod5/guessNumber.c
@@ -20,7 +20,8 @@
 #include <time.h>    // time()
 
 // Constants
-#define MAX 3
+#define MAX 5
+#define LIMIT 100
 // Function Prototypes
 
 // Main Function
@@ -28,13 +29,23 @@ int main()
 {
     int number, guess;
     srand(time(0));       // set the random SEED
-    guess = rand() %10;   // set random number between 0-9
+    guess = rand() % LIMIT;   // set random number between 0-9, changed to LIMIT
     //int i;   // before C89 you need to define i outside loop
     for(int i = 0; i < MAX; i++)
     {
-        printf("Please guess a number between 0 and 10\n");
+        printf("Please guess a number between 0 and %d\n", LIMIT);
+        printf("You have %d opportunities left\n", MAX - i);
         scanf("%d", &number);
-        if(number == guess)
+
+        if(number > guess)
+        {
+            printf("Guess lower\n");
+        }
+        else if(number < guess)
+        {
+            printf("Guess higher\n");
+        }
+        else
         {
             printf("Congratulations, you got the correct number\n");
             return 0;
