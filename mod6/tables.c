7ffdf081493e3943ab32e3c72a55953fad6dcbb7
Brenton Ray <brentonray@mail.weber.edu>
2019-02-12 10:07:11 -0700

    added 2D array for table values

--- mod6/tables.c
+++ mod6/tables.c
@@ -18,23 +18,25 @@
 #include <stdio.h>
 
 // Constants
-#define TABLES 10
+#define TABLES 12
 // Function Prototypes
 
 // Main Function
 int main()
 {
-    
+    // 2D array [row][column]
+    int tables[TABLES][TABLES];
     for(int i = 1; i <= TABLES; i++)
     {
         for(int j = 1; j <= TABLES; j++)
         {
             printf("%3d\t", i * j);
+            tables[i-1][j-1] = i * j;     // index notation
         }
         printf("\n");
     }
-
-
+    printf(" 1 * 6 = %d\n", tables[0][5]);
+    printf(" 2 * 8 = %d\n", tables[1][7]);
 
     return 0;
 }
