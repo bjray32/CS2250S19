bf228a2f7be623cd3b00fc6274c721f2375346d8
Brenton Ray <brentonray@mail.weber.edu>
2019-02-12 09:49:17 -0700

    averages and days of min and max

--- mod6/temperatures.c
+++ mod6/temperatures.c
@@ -19,7 +19,7 @@
 #include <time.h>
 #include <stdlib.h>
 // Constants
-#define NUM 40
+#define NUM 4
 #define MIN 20   // min temp
 #define MAX 45   // max temp
 // Function Prototypes
@@ -29,9 +29,11 @@ int main()
 {
     double max_ogden[NUM];   // posting NUM values for ogden
     double min_ogden[NUM];
+    double avg = 0, min_temp = 0, max_temp = 0;
+    int min_day = 1, max_day = 1;
     srand(time(0));     // set random seed
     // Task: Capture max and min temps from user
-    printf("Simulating the max and min temperatures of Ogden for the nex %d days\n ", NUM);
+    printf("Simulating the max and min temperatures of Ogden for the next %d days\n ", NUM);
     for(int i = 0; i < NUM; i++)
     {
 //        printf("%d day max and min\n", i+1);
@@ -48,11 +50,28 @@ int main()
     }
 
     // Task: Calcuate the average and min and max of the date range
+    // Initialize your min_temp and max_temp
+    min_temp = min_ogden[0];
+    max_temp = max_ogden[0];
     for(int i = 0; i < NUM; i++)
     {
-
-        
+        avg += min_ogden[i];    // adding elements
+        avg += max_ogden[i];
+        if(min_temp > min_ogden[i])
+        {
+            min_temp = min_ogden[i];
+            min_day = i + 1;
+        }
+        if(max_temp < max_ogden[i])
+        {
+            max_temp = max_ogden[i];
+            max_day = i + 1;
+        }
     }
+    avg = avg/(NUM * 2);       // calculating avg
+    printf("Your average temperature = [%6.2lf]\n", avg);
+    printf("Your min temp [%6.2lf] on day [%02d]\n", min_temp, min_day);
+    printf("Your max temp [%6.2lf] on day [%2d]\n", max_temp, max_day);
     
         
     return 0;
