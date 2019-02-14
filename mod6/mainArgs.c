da165fd10e1b8f59a9d180262b50b4cea5d6bd1b
Brenton Ray <brentonray@mail.weber.edu>
2019-02-14 09:28:16 -0700

    inital commit

---
+++ mod6/mainArgs.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mainArgs.c
+ *
+ *    Description:  Pass arguments/parameters to main function
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2019 09:17:23 AM
+ *       Revision:  none
+ *       Compiler:  gcc mainArgs.c -o main Args.out
+ *
+ *         Author:  Brenton Ray (), brentonray@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    printf("Argument count is: %d\n", argc); 
+    printf("Argv 0 [%s]\n", argv[0]);
+
+    for (int i = 0; i < argc; i +=1)
+    {
+        printf("Argc %d [%s]\n", i, argv[i]);
+    }
+
+
+    return 0;
+}
+// Function Definitions
+
+
