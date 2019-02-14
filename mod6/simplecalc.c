76fecac88bbff6d9ffbb96df792d52659a2abe73
Brenton Ray <brentonray@mail.weber.edu>
2019-02-14 10:18:53 -0700

    do while loop

--- mod6/simplecalc.c
+++ mod6/simplecalc.c
@@ -17,17 +17,19 @@
  * =====================================================================================
  */
 #include <stdio.h>
-
+#include <stdlib.h>  // atoi() string to integer
 // Constants
-#define ADD 1
-#define SUB 2
-#define MUL 3
+#define EXIT 0
+#define ADD  1
+#define SUB  2
+#define MUL  3
 // Function Prototypes
 
 // Main Function
 int main(int argc, char* argv[])
 {
     int choice = 0;
+    int num1, num2;
     // Validate argument count, return 1 otherwise
 
     if(argc != 3)
@@ -36,26 +38,38 @@ int main(int argc, char* argv[])
         printf("Usage: ./%s <PARAM1> <PARAM2>\n", argv[0]);
         return 1;
     }
-    printf("What do you want to do with these numbers?\n");
-    printf("\t 1) Addition\n");
-    printf("\t 2) Subtration\n");
-    printf("\t 3) Multiplication\n");
-    scanf("%d", &choice);
+    // Convert input strings into integers
+    num1 = atoi(argv[1]);  // first input param
+    num2 = atoi(argv[2]);  // second input param
 
-    switch (choice) 
+    do  // beginning of do while
     {
-        case ADD:
-            break;
 
-        case SUB:
-            break;
+        printf("What do you want to do with these numbers?\n");
+        printf("\t 1) Addition\n");
+        printf("\t 2) Subtration\n");
+        printf("\t 3) Multiplication\n");
+        printf("\t 0) To Quit\n");
+        scanf("%d", &choice);
+        
+        switch (choice) 
+        {
+            case ADD:
+                printf("The sum of %d + %d is %d\n", num1, num2, num1 + num2);
+                break;
 
-        case MUL:
-            break;
+            case SUB:
+                printf("The difference of %d - %d is %d\n", num1, num2, num1 - num2);
+                break;
 
-        default:
-           printf("Unsupported function\n");
-    }				/* -----  end switch  ----- */
+            case MUL:
+                printf("The multiplication of %d * %d is %d\n", num1, num2, num1 * num2);
+                break;
+
+            default:
+             printf("Unsupported function\n");
+        }
+        } while(choice != EXIT); // end of do while
 
     printf("Adios amigo\n");
     return 0;
