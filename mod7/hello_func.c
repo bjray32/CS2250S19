bbd6339acc54551767c21ef1286a095ab7a2b844
Brenton Ray <brentonray@mail.weber.edu>
2019-02-19 09:16:37 -0700

    add Get_squared function, not working...

--- mod7/hello_func.c
+++ mod7/hello_func.c
@@ -22,13 +22,17 @@
 // Function Prototypes
 void Hello_World();
 void Hello_Number(int num);
+int Get_Square(int num);
 
 
 // Main Function
 int main(int argc, char* argv[])
 {
+    int i1 = 23;
     Hello_World();
-    Hello_Number(23);
+    Hello_Number(i1);
+    i1 = Get_Square(i1);
+    Hello_Number(i1);
 
     return 0;
 }
@@ -65,8 +69,23 @@ void Hello_Number(int num)
 }
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Get_Squared
+ *  Description:  Get the square of an integer number
+ *       Param1: num => integer value
+ *       Return: the square of num
+ * =====================================================================================
+ */
 
+int Get_Squared(int num)
+{
+    int square;
+    //Allgorithm
+    square = num * num;
 
+    return square;
+}
 
 
 
