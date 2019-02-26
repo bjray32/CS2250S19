31961b6c7190c8d79917e86b681530286da4572c
Brenton Ray <brentonray@mail.weber.edu>
2019-02-26 08:59:32 -0700

    inital commit

---
+++ mod8/by_address.c
@@ -0,0 +1,97 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  by_address.c
+ *
+ *    Description:  Sending information to functions?
+ *
+ *        Version:  1.0
+ *        Created:  02/26/2019 08:37:03 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Brenton Ray (), brentonray@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constants
+
+// Function Prototypes
+void DisplayPosition(double x, double y);
+void UpdatePosition(double x, double y, double new_x, double new_y);
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    double x, y;
+    if(argc != 3)
+    {
+
+    printf("Usage: %s <X_POS> <Y_POS>\n", argv[0]);
+    return 1;
+
+    }
+    x = atof(argv[1]);  // Convert the first input param to double
+    y = atof(argv[2]);  // Convert the second input param to double
+    DisplayPosition(x, y);
+    UpdatePosition(x, y, 2, 4);
+    DisplayPosition(x, y);
+
+    return 0;
+}
+// Function Definitions
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  DisplayPosition
+ *  Description:  Display current point position
+ *        Param:  x => x coordinate
+ *        Param:  y => y coordinate
+ *      Returns:  N/A
+ * =====================================================================================
+ */
+void DisplayPosition(double x, double y)
+{
+    printf("Your initial position is (%4.2lf, %4.2lf)\n", x, y);
+    return;
+}
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  UpdatePosition
+ *  Description:  Update point point position to a new position
+ *        Param:  x => x coordinate
+ *        Param:  y => y coordinate
+ *        Param:  x_new => new x coordinate
+ *        Param:  y_new => new y coordinate
+ *      Returns:  N/A
+ * =====================================================================================
+ */
+void UpdatePosition(double x, double y, double new_x, double new_y)
+{
+    printf("******Updating Position*******\n");
+    DisplayPosition(x, y);
+    x = new_x;
+    y = new_y;
+    DisplayPosition(x, y);
+    printf("******Done Updating Position*******\n");
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
