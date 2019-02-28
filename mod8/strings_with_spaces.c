b28853beaeb58a59760728182591df734c87a99d
Brenton Ray <brentonray@mail.weber.edu>
2019-02-28 09:38:28 -0700

    initial commit, i think

--- mod8/strings_with_spaces.c
+++ mod8/strings_with_spaces.c
@@ -18,11 +18,13 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>  // strlen()
+#include <ctype.h>   // islower(), toupper()
 
 // Constants
 const int INPUT_SIZE = 50;        // Same as #define
 // Function Prototypes
+void StrSpaceToHyphen(char inputStr[]);
 
 // Main Function
 int main(int argc, char* argv[])
@@ -39,9 +41,35 @@ int main(int argc, char* argv[])
     {
         printf("%d char is [%c]\n", i, user_input[i]);
     }
+    
+    StrSpaceToHyphen(user_input);
+    printf("New String [%s]\n", user_input);
 
     return 0;
 }
 // Function Defenitions
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  StrSpaceToHyphen
+ *  Description:  Relplace all spaces with hyphens and change lower case to upper
+ *        Param:  inputStr: input string with spaces
+ * =====================================================================================
+ */
+void StrSpaceToHyphen(char inputStr[])
+{
+    for(int i = 0; i < strlen(inputStr); i++)
+    {
+        if(inputStr[i] == ' ')
+        {
+            inputStr[i] = '-';
+        }
+        else if(islower(inputStr[i]))
+        {
+            inputStr[i] = toupper(inputStr[i]);
+        }
+    }
+
+    return;
+}
